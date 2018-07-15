c9c88d244b396a66f32c6d6722d6f9b9f237bfba
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2018-07-15 15:08:48 +0200

    Initial Commit

---
+++ SmartCardReaderARC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartCardReaderARC
+//
+//  Created by Andrej Trajkovski on 7/15/18.
+//  Copyright © 2018 Andrej Trajkovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
